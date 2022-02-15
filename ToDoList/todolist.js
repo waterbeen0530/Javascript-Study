c46aff9aa5b71db2d427b7dcd5dff050e4808b00
waterbeen0530 <hange253@gmail.com>
2022-02-15 23:01:36 +0900

    [CREATE] ToDoList
    todolist - 등록, 저장 기능 구현

---
+++ ToDoList/todolist.js
@@ -0,0 +1,55 @@
+const toDoFrom = document.querySelector(".js-toDoFrom"),
+      toDoInput = toDoFrom.querySelector("input"),
+      toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null){
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo){
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+
+function init() {
+  loadToDos();
+  toDoFrom.addEventListener("submit", handleSubmit)
+}
+
+init();
