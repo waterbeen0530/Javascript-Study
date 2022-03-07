c6a5e37e6c9f8c496ce8f7e4d20d41f29e0e4e10
waterbeen0530 <hange253@gmail.com>
2022-03-07 19:31:10 +0900

    [UPDATE] input test
    todolist - input 미기제시 경고창 띄우기

--- ToDoList/todolist-Ver3/todolist-Ver3.js
+++ ToDoList/todolist-Ver3/todolist-Ver3.js
@@ -1,1 +1,19 @@
-alert("Hello");
+window.addEventListener('load', ()=>{
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_el = document.querySelector("#tasks");
+
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const task = input.value;
+
+    if (!task){
+      alert("내용을 입력해주세요.");
+    }
+    else{
+      console.log("success");
+    }
+  })
+
+})
