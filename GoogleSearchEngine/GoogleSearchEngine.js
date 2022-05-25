3fd35a45122c8fd6b6c7e41ebd9fc799c3d2b9c4
waterbeen0530 <hange253@gmail.com>
2022-05-25 23:25:49 +0900

    [UPDATE] GoogleSearchEngine
    엔터시 focus 이동 구현

--- GoogleSearchEngine/GoogleSearchEngine.js
+++ GoogleSearchEngine/GoogleSearchEngine.js
@@ -1,7 +1,16 @@
-let query = document.querySelector(".query");
-let searchBtn = document.querySelector(".searchBtn");
+let searchBtn = document.getElementById("searchBtn");
+let as = document.getElementById("as");
 
-searchBtn.onclick = function () {
-  let url = "https://www.google.com/search?q=" + query.value;
+const openWebsite = () => {
+  let url = "https://www.google.com/search?q=" + as.value;
   window.open(url);
 };
+
+searchBtn.addEventListener("click", openWebsite);
+
+function handleEnter(event) {
+  if (event.keyCode == 13) {
+    openWebsite();
+  }
+}
+as.addEventListener("keydown", handleEnter);
