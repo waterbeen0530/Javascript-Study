36b35a31653c5986b17302f8f27f1cee2d9ec1d2
waterbeen0530 <hange253@gmail.com>
2022-05-27 23:48:47 +0900

    [UPDATE] Modify variable name
    변수명 수정

--- GoogleSearchEngine/GoogleSearchEngine.js
+++ GoogleSearchEngine/GoogleSearchEngine.js
@@ -1,16 +1,16 @@
-let searchBtn = document.getElementById("searchBtn");
-let as = document.getElementById("as");
+const searchBtn = document.getElementById("searchBtn");
+const as = document.getElementById("as");
 
-const openWebsite = () => {
+const openGoogle = () => {
   let url = "https://www.google.com/search?q=" + as.value;
   window.open(url);
 };
 
-searchBtn.addEventListener("click", openWebsite);
+searchBtn.addEventListener("click", openGoogle);
 
 function handleEnter(event) {
   if (event.keyCode == 13) {
-    openWebsite();
+    openGoogle();
   }
 }
 as.addEventListener("keydown", handleEnter);
