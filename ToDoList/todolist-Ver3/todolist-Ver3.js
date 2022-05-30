971aaa183c1f555bde4d090350b9f0a50274ad3d
waterbeen0530 <hange253@gmail.com>
2022-05-30 23:17:19 +0900

    [CREATE] ToDoList
    ToDoList 등록 삭제 구현

--- ToDoList/todolist-Ver3/todolist-Ver3.js
+++ ToDoList/todolist-Ver3/todolist-Ver3.js
@@ -1,19 +1,49 @@
-window.addEventListener('load', ()=>{
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task-input");
-  const list_el = document.querySelector("#tasks");
+"use strict";
+let itemList = [];
 
-  form.addEventListener('submit', (e)=>{
-    e.preventDefault();
+let inputButotn = document.getElementById("input_btn");
+input_btn.addEventListener("click", addItem);
 
-    const task = input.value;
+function addItem() {
+  let item = document.getElementById("item").value;
+  if (item !== null) {
+    itemList.push(item);
+    document.getElementById("item").value = "";
+    document.getElementById("item").focus;
+  }
+  showList();
+}
 
-    if (!task){
-      alert("내용을 입력해주세요.");
-    }
-    else{
-      console.log("success");
-    }
-  })
+function showList() {
+  let list = "<ul>";
+  for (let i = 0; i < itemList.length; i++) {
+    list +=
+      "<li>" +
+      itemList[i] +
+      "<span class='close' id=" +
+      i +
+      ">" +
+      "❌" +
+      "</span></li>";
+  }
+  list += "</ul>";
+  document.querySelector("#item_list").innerHTML = list;
 
-})
+  let deleteButtons = document.querySelectorAll(".close");
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", deleteItem);
+  }
+}
+
+function deleteItem() {
+  let id = this.getAttribute("id");
+  itemList.splice(id, 1);
+  showList();
+}
+
+function handleEnter(event) {
+  if (event.keyCode === 13) {
+    addItem();
+  }
+}
+item.addEventListener("keydown", handleEnter);
